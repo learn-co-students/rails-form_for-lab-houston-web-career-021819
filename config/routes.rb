d4025e341e513ee499c2e738ee9522d180c57a20
github username <github email address>
2019-03-17 15:49:48 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+resources :students 
+resources :school_classes 
+post '/school_classes/new', to: 'school_classes#create', as: 'new'
+post '/students/new', to: 'students#create', as: 'create_new_student'  #check rails routes and make sure route names are different!
+end 
+     
